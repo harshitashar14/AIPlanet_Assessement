ae561f1cf61713a6d65393bcc5f7adb13ffad31f
Harshita <harshitashar14@gmail.com>
2023-04-04 15:07:39 +0530

    with env

--- django_assessment/settings.py
+++ django_assessment/settings.py
@@ -17,17 +17,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-thtt4_l^%jv+dvpy9=rmknfic-wndd)dgfyrb6_$#bf$csk1u@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Application definition
 
